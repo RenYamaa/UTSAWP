dc3d4abad22961a3281b0fd1103ecf6aa69fdcbe
Michael <michaelgrowt24@gmail.com>
2025-10-21 00:29:35 +0700

    Change Connect Route

--- routes/ConnectRoute.js
+++ routes/ConnectRoute.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'comifuro'
+    host: process.env.MYSQLHOST,     
+    user: process.env.MYSQLUSER,     
+    password: process.env.MYSQLPASSWORD, 
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT        
 });
 
 connection.connect((err) =>{
     if(err) {
         console.error('Error Connection to Database: ' + err);
+        return;
     }
     console.log('Connected to Database!');
 });
